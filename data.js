a8f0c33ae244bb8c76dafa0d7f8521e7c041c430
victmartinez <victmartinez@outlook.com>
2019-05-16 11:29:17 -0400

    .filter

---
+++ data.js
@@ -0,0 +1,26 @@
+const students = [
+    {
+        name: 'Alvaro',
+        score: 10,
+    },
+    {
+        name: 'Daniel',
+        score: 16,
+    },
+    {
+        name: 'Alexys',
+        score: 12,
+    },
+    {
+        name: 'Rafa',
+        score: 17,
+    },
+    {
+        name: 'Alejandro',
+        score: 8,
+    },
+    {
+        name: 'Sofia',
+        score: 9,
+    }
+]
