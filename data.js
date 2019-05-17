5a15473268f21ea71002edb147f45c23bb454ae2
Victoria Martínez <48461290+victmartinez@users.noreply.github.com>
2019-05-17 02:51:37 -0400

    Update data.js

--- data.js
+++ data.js
@@ -25,29 +25,24 @@ const students = [
     }
 ]
     
-const students = [
-    {
-        name: 'Alvaro',
-        score: 10,
-    },
-    {
-        name: 'Daniel',
-        score: 16,
-    },
-    {
-        name: 'Alexys',
-        score: 12,
-    },
-    {
-        name: 'Rafa',
-        score: 17,
-    },
-    {
-        name: 'Alejandro',
-        score: 8,
-    },
-    {
-        name: 'Sofia',
-        score: 9,
-    }
-]
+    
+const KIDS = {
+  "kids": [{
+    "name": "Alicia",
+    "lastName": "Díaz",
+    "type": "student",
+    "hobby": "Cantar" 
+  },
+  {
+    "name": "Ale",
+    "lastName": "Mendez",
+    "type": "student",
+    "hobby": "Andar en bicicleta" 
+  },
+  {
+    "name": "Rosa",
+    "lastName": "Espinoza",
+    "type": "student",
+    "hobby": "Comer" 
+  }]
+}
