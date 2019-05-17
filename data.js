84999e358c5e5ca338a9bdeec91b96a587c88df7
Victoria Martínez <48461290+victmartinez@users.noreply.github.com>
2019-05-17 02:50:00 -0400

    Update data.js

--- data.js
+++ data.js
@@ -24,3 +24,30 @@ const students = [
         score: 9,
     }
 ]
+    
+const students = [
+    {
+        name: 'Alvaro',
+        score: 10,
+    },
+    {
+        name: 'Daniel',
+        score: 16,
+    },
+    {
+        name: 'Alexys',
+        score: 12,
+    },
+    {
+        name: 'Rafa',
+        score: 17,
+    },
+    {
+        name: 'Alejandro',
+        score: 8,
+    },
+    {
+        name: 'Sofia',
+        score: 9,
+    }
+]
